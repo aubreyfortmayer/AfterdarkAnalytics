35c0cc1561fa568394188a3458d189353cf8752d
Angelica Hanley <angelicahanley2@gmail.com>
2026-01-22 21:56:40 -0500

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p className="text-pink-500 font-extrabold text-2xl">
+        AfterDark Analytics Home Page ! ! ! &lt;3
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
