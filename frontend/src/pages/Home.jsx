91e1d74de3b34545c2f30a772a087ab650a6b607
Angelica Hanley <angelicahanley2@gmail.com>
2026-02-09 23:24:11 -0500

    feat: updated home page to be responsive and added intro section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,35 @@
 import Title1 from "../assets/AfterDarkText.png";
 import Title2 from "../assets/AnalyticsText.png";
+import IntroSection from "../assets/IntroSection.png";
 
 export default function Home() {
     return (
-        <div className="flex-col items-center -mt-22">
+        //Section 1: Title, utilizing clamp to make it responsive for different screens
+        <div className="flex flex-col items-center -mt-[clamp(24px,10vw,50px)]">
             <img 
                 src={Title1}
                 alt=""
-                className=""
+                className="self-start pl-[clamp(10px,2vw,24px)] h-[clamp(200px,32vw,500px)]"
             />
+        
+        <div className="flex justify-end w-full pr-6">
             <img 
                 src={Title2}
                 alt=""
-                className="-mt-32"
+                className="-mt-[clamp(80px,16vw,200px)] h-[clamp(100px,22vw,500px)]"
             />
+        </div>
+
+        {/*Section 2: Intro*/}
+        <div className="flex justify-end w-full">
+            <img 
+                src={IntroSection}
+                alt=""
+                className="-mt-[clamp(80px,16vw,200px)] h-[clamp(200px,60vw,800px)]"
+            />
+
+        </div>
+
         </div>
     );
 }
--- frontend/src/pages/Results.jsx
+++ frontend/src/pages/Results.jsx
@@ -1,6 +1,6 @@
 export default function Results() {
     return (
-        <div className="flex items-center">
+        <div className="flex items-center justify-center">
             <p className="text-pink-500 font-extrabold text-2xl">
             AfterDark Analytics Results Page ! ! ! &lt;3
             </p>
