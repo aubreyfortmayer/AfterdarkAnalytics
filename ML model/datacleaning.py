27b897ad0cf928ecbd2ed2bf7a7409e0bd65e3a0
jadevega <116754119+jadevega@users.noreply.github.com>
2026-02-19 13:05:27 -0500

    Dropped demographic columns, encoded Target Variable

--- ML model/datacleaning.py
+++ ML model/datacleaning.py
@@ -1,3 +1,18 @@
 import pandas as pd
-df = pd.read_csv("survey.csv");
+from sklearn.preprocessing import LabelEncoder
+readindf = pd.read_csv("survey.csv");
+names_of_og_cols = readindf.columns
+print(names_of_og_cols);
+readindf.drop(["Timestamp", "Name", "Current Year at UF", "Gender", "Age Range"], axis=1, inplace=True);
+name_of_cols_no_dem = readindf.columns;
+print(name_of_cols_no_dem);
+
+#seperating target column and turning it into numerical value.
+readindf["Target"] = readindf["Are you going out tonight?"].map({"Yes": 1, "No": 0})
+readindf.drop(["Are you going out tonight?"], axis = 1, inplace=True);
+print(readindf);
+
+target_col = readindf["Target"];
+print(target_col);
+
 
