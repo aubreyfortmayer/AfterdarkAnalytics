f671bd0d9eebfbceae550e510704666081288562
aubreyfortmayer <aubreyfortmayer@ufl.edu>
2026-04-12 14:26:29 -0400

    got rid of user router

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import predict_routers, rec_routers, resp_routers, user_routers
+from routers import rec_routers, resp_routers
 
 app = FastAPI()
 
-app.include_router(predict_routers.router)
 app.include_router(rec_routers.router)
 app.include_router(resp_routers.router)
-app.include_router(user_routers.router)
-
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,7 +14,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
